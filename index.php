a488a618d6e1ca90ce8a958f553c008cc4a33d30
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-05-09 21:24:26 -0400

    Estilizando la cabecera y el menu, tambien agregando div class clearfix para limpiar css

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
                   </li>
               </ul>
           </nav>
+          <div class="clearfix"></div>
         </header>
         <div id="container">
             <!--BARRA LATERAL-->
