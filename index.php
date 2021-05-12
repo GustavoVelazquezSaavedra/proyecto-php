625ba0db65f63184e7821c37f1f80ea59f9a27a6
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-05-12 11:23:16 -0400

    Agregando id ver-todas enfasis entradas, y clearfix despues del final e la parte principal de la pagina

--- index.php
+++ index.php
@@ -76,45 +76,55 @@
             <div id="principal">
                 <h1>Ultimas Entradas</h1>
                 <article class="tickets">
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum."
-                    </p>
+                    <a href="#">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                            mollit anim id est laborum."
+                        </p>
+                    </a>
                 </article>
                 
                 <article class="tickets">
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum."
-                    </p>
+                    <a href="#">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                            mollit anim id est laborum."
+                        </p>
+                    </a>
                 </article>
                 
                 <article class="tickets">
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum."
-                    </p>
+                    <a href="#">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                            mollit anim id est laborum."
+                        </p>
+                    </a>
                 </article>
+                <div id="ver-todas">
+                    <a href="#">Ver todas las entradas</a>
+                </div>
                 
-            </div>
+            </div><!--FIN PRINCIPAL-->
+            <div class="clearfix"></div>
         </div>
        
 
