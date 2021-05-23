d8aacbfd96e6ec938d5cbe782b19bb358224d63e
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-05-23 08:39:59 -0400

    Creando el archivo para hacer login con lo necesario para identificarse

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php 
+
+// START SESSION AND DATABASE CONEC.
+require_once './includes/conexion.php';
+
+// GET FORM DATA
+if(isset($_POST)){
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    // DATA VALIDATE
+    $sql = "SELECT * FROM  usuarios WHERE email = '$email'";
+    $login = mysqli_query($db, $sql);
+
+    if($login && mysqli_num_rows($login) == 1){
+        // check the database if the data entered is correct
+        $usuario = mysqli_fetch_assoc($login);
+        
+        //password verify
+        $verify = password_verify($password, $usuario['password']);
+
+        if($verify){
+            // use a session for save userdata logged
+            $_SESSION['usuario']= $usuario;
+            if(isset($_SESSION['error_login'])){
+                unset($_SESSION['error_login']);
+            }
+        }else{
+            //error
+            $_SESSION['error_login'] = "Login incorrecto!. ";
+        }
+
+    }else{
+        // fails messege
+        //error
+        $_SESSION['error_login'] = "Login incorrecto!. ";
+
+    }
+
+
+ 
+}
+
+// is fail send session with fails, and redirect
+header('Location: index.php');
+
+?>
