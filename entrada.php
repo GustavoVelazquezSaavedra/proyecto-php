45e6e7609c9aac9030a493eeba3a6ddaa613632b
Gustavo Velazquez <gabrivelaz@hotmail.com>
2021-06-06 15:10:24 -0400

    Agregando boton de editar entradas y/o borrarlos en caso de que si la entrada pertenece al usuario logueado

--- entrada.php
+++ entrada.php
@@ -1,11 +1,11 @@
-<?php require_once'./includes/conexion.php';?>
-<?php require_once'./includes/helpers.php';?>
-<?php 
-        $entrada_actual = conseguirEntrada($db,$_GET['id']);
+<?php require_once './includes/conexion.php'; ?>
+<?php require_once './includes/helpers.php'; ?>
+<?php
+$entrada_actual = conseguirEntrada($db, $_GET['id']);
 
-        if(!isset($entrada_actual['id'])){
-            header("Location: index.php");
-        }
+if (!isset($entrada_actual['id'])) {
+    header("Location: index.php");
+}
 ?>
 
 <?php require_once './includes/header.php'; ?>
@@ -13,15 +13,21 @@
 
 <!--CAJA PRINCIPAL-->
 <div id="principal">
-    
-    <h1><?=$entrada_actual['titulo']?></h1>
-    <a href="categoria.php?id=<?=$entrada_actual['categoria_id']?>">
-        <h2><?=$entrada_actual['categoria']?></h2>
+
+    <h1><?= $entrada_actual['titulo'] ?></h1>
+    <a href="categoria.php?id=<?= $entrada_actual['categoria_id'] ?>">
+        <h2><?= $entrada_actual['categoria'] ?></h2>
     </a>
-    <h4><?=$entrada_actual['fecha']?></h4>
+    <h4><?= $entrada_actual['fecha'] ?> | <?=$entrada_actual["usuario"]?></h4>
     <p>
-    <?=$entrada_actual['descripcion']?>
+        <?= $entrada_actual['descripcion'] ?>
     </p>
+
+    <?php if (isset($_SESSION["usuario"]) && $_SESSION["usuario"]["id"] == $entrada_actual["usuario_id"]) : ?>
+        <br>
+        <a href="./editar-entrada.php" class="boton boton-naranja">Editar Entrada</a>
+        <a href="./borrar-entrada.php" class="boton boton-rojo">Borrar Entrada</a>
+    <?php endif; ?>
 </div>
 <!--FIN PRINCIPAL-->
 
